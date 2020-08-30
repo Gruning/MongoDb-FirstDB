83f2f477e8283e04917c6381661815bfc46f038e
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-08-30 16:53:09 -0300

    sec8cap110_creatingSimpleBackend_connectionString

--- mongo.js
+++ mongo.js
@@ -1,3 +1,7 @@
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb+srv://gruningzen:Esfera3010@cluster0.uo1tr.mongodb.net/products_test?retryWrites=true&w=majority'
+
 const createProduct = async (req,res,next)=>{
 
 }
