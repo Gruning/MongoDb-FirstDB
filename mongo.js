7ac034cb0237b4b900b6de0e73797ccbce538ff4
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-08-31 20:32:23 -0300

    connection to client

--- mongo.js
+++ mongo.js
@@ -3,7 +3,12 @@ const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb+srv://gruningzen:Esfera3010@cluster0.uo1tr.mongodb.net/products_test?retryWrites=true&w=majority'
 
 const createProduct = async (req,res,next)=>{
-
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price,
+    }
+    
+    const client = new MongoClient(url)
 }
 
 const getProducts = async(req, res,next)=>{
