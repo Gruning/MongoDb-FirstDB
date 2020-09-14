2d7da8c786da75e9ee3715562b3b592d8c31a360
Alberto Gruning <alberto.gruning.zen@gmail.com>
2020-09-14 19:38:57 -0300

    sec8cap115_creatingProduct

--- models/product.js
+++ models/product.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
-    name: { type: String ,required:true },
-    price: { type: number, required: true }
+    name: { type: Strin},
+    price: { type: numb},
 })
 
 module.exports = mongoose.model('Product', productSchema)
